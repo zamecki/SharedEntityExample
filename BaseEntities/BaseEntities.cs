822a8e7a98462c6d6f3265f5d1d6fa35968c0274
Lucas.Zamecki@equusoft.com <lucas.zamecki@equusoft.com>
2023-08-03 12:08:53 +0100

    Init

---
+++ BaseEntities/BaseEntities.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaseEntities;
+public interface IBaseEntity
+{
+    public Guid Id { get; }
+}
+public abstract class CommonPersonProfile : IBaseEntity
+{
+    public Guid Id { get; set; }
+    public Guid? UserAccountId { get; set; }
+    public virtual CommonUserAccount UserAccount  { get; protected set; } = default!;
+}
+
+
+public abstract class CommonUserAccount : IBaseEntity
+{
+    public Guid Id { get; set; }
+    public virtual CommonPersonProfile PersonProfile { get; protected set; } = default!;
+}
+
+
+public class CommonDbContext : DbContext
+{
+    public DbSet<CommonUserAccount> UserAccounts { get; set; }
+    public DbSet<CommonPersonProfile> PeopleProfiles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=Example;Trusted_Connection=True;MultipleActiveResultSets=true;encrypt=false;");
+    }
+}
---
+++ SharedEnitityExample/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BaseEntities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+using var dbContext = new ExampleDbContext();
+var result = dbContext.PeopleProfiles
+                        .Include(t => t.UserAccount)
+                            .ThenInclude(t => t.Employee)
+                        .ToList();
+
+public class PersonProfile : CommonPersonProfile
+{
+    public new UserAccount? UserAccount => (UserAccount?)base.UserAccount;
+    public string? Message { get; set; }
+}
+
+public class UserAccount : CommonUserAccount
+{
+    public Guid? EmployeeId { get; set; }
+    public virtual Employee? Employee { get; set; }
+    public new virtual PersonProfile PersonProfile => (PersonProfile)base.PersonProfile;
+}
+public class Employee : IBaseEntity
+{
+    public Guid Id { get; set; }
+    public string? EmployeeNumber { get; set; }
+}
+
+public class ExampleDbContext : CommonDbContext
+{
+    public new DbSet<UserAccount> UserAccounts { get; set; } = default!;
+    public new DbSet<PersonProfile> PeopleProfiles { get; set; }
+}
+
+
+
+
